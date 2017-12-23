b7f12c7f27b84198515b64f482cb44d8309d4173
Isterikus <kovalenko27071996@gmail.com>
2017-12-23 18:36:59 +0200

    testing...

--- model.py
+++ model.py
@@ -14,14 +14,13 @@ data = read_csv("data/train.csv")
 data.Age = data.Age.median()
 MaxPassEmbarked = data.groupby('Embarked').count()['PassengerId']
 data.Embarked[data.Embarked.isnull()] = MaxPassEmbarked[MaxPassEmbarked == MaxPassEmbarked.max()].index[0]
-data.PassengerId[data.Fare.isnull()]
-data = data.drop(['PassengerId','Name','Ticket','Cabin'],axis=1)
+data = data.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
 label = LabelEncoder()
 dicts = {}
 
-label.fit(data.Sex.drop_duplicates()) #задаем список значений для кодирования
+label.fit(data.Sex.drop_duplicates())
 dicts['Sex'] = list(label.classes_)
-data.Sex = label.transform(data.Sex) #заменяем значения из списка кодами закодированных элементов 
+data.Sex = label.transform(data.Sex)
 
 label.fit(data.Embarked.drop_duplicates())
 dicts['Embarked'] = list(label.classes_)
@@ -29,7 +28,7 @@ data.Embarked = label.transform(data.Embarked)
 
 test = read_csv('data/test.csv')
 test.Age[test.Age.isnull()] = test.Age.mean()
-test.Fare[test.Fare.isnull()] = test.Fare.median() #заполняем пустые значения средней ценой билета
+test.Fare[test.Fare.isnull()] = test.Fare.median()
 MaxPassEmbarked = test.groupby('Embarked').count()['PassengerId']
 test.Embarked[test.Embarked.isnull()] = MaxPassEmbarked[MaxPassEmbarked == MaxPassEmbarked.max()].index[0]
 result = DataFrame(test.PassengerId)
@@ -42,9 +41,10 @@ label.fit(dicts['Embarked'])
 test.Embarked = label.transform(test.Embarked)
 
 target = data.Survived
-train = data.drop(['Survived'], axis=1) #из исходных данных убираем Id пассажира и флаг спасся он или нет
-kfold = 5 #количество подвыборок для валидации
-itog_val = {} #список для записи результатов кросс валидации разных алгоритмов
+train = data.drop(['Survived'], axis=1) # из исходных данных убираем Id пассажира и флаг спасся он или нет
+kfold = 5 # количество подвыборок для валидации
+itog_val = {} # список для записи результатов кросс валидации разных алгоритмов
+
 
 def parse(data):
 	label = LabelEncoder()
@@ -57,7 +57,7 @@ def parse(data):
 	dicts['Embarked'] = list(label.classes_)
 
 	data.Age[data.Age.isnull()] = data.Age.mean()
-	data.Fare[data.Fare.isnull()] = data.Fare.median() #заполняем пустые значения средней ценой билета
+	data.Fare[data.Fare.isnull()] = data.Fare.median() # заполняем пустые значения средней ценой билета
 	MaxPassEmbarked = data.groupby('Embarked').count()['PassengerId']
 	data.Embarked[data.Embarked.isnull()] = MaxPassEmbarked[MaxPassEmbarked == MaxPassEmbarked.max()].index[0]
 	result = DataFrame(data.PassengerId)
@@ -69,6 +69,7 @@ def parse(data):
 	label.fit(dicts['Embarked'])
 	data.Embarked = label.transform(data.Embarked)
 	return data
+
 
 def crossValidation(model_rfc, model_knc, model_svc, model_lr):
 	scores = cross_validation.cross_val_score(model_rfc, train, target, cv = kfold)
@@ -84,27 +85,28 @@ def crossValidation(model_rfc, model_knc, model_svc, model_lr):
 
 	plt.show()
 
+
 def rocCurve(model_rfc, model_knc, model_svc, model_lr):
 	ROCtrainTRN, ROCtestTRN, ROCtrainTRG, ROCtestTRG = cross_validation.train_test_split(train, target, test_size=0.25)
 	pl.clf()
-	plt.figure(figsize=(8,6))
-	#SVC
+	plt.figure(figsize=(8, 6))
+	# SVC
 	model_svc.probability = True
 	probas = model_svc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
 	fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1])
 	roc_auc  = auc(fpr, tpr)
 	pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('SVC', roc_auc))
-	#RandomForestClassifier
+	# RandomForestClassifier
 	probas = model_rfc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
 	fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1])
 	roc_auc  = auc(fpr, tpr)
 	pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('RandonForest',roc_auc))
-	#KNeighborsClassifier
+	# KNeighborsClassifier
 	probas = model_knc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
 	fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1])
 	roc_auc  = auc(fpr, tpr)
 	pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('KNeighborsClassifier',roc_auc))
-	#LogisticRegression
+	# LogisticRegression
 	probas = model_lr.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
 	fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1])
 	roc_auc  = auc(fpr, tpr)
